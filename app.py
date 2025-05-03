2fc78de277323e89235b6470d0096ae2fd994128
Harisanthanaselvam <152364843+Harisanthanaselvam@users.noreply.github.com>
2025-05-03 13:01:42 +0530

    Your commit message

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import pickle
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
 
 
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
+
 
 label_to_crop = {
     0: 'apple', 1: 'banana', 2: 'blackgram', 3: 'chickpea',
@@ -14,7 +17,13 @@ label_to_crop = {
     20: 'rice', 21: 'watermelon'
 }
 
+
 app = Flask(__name__)
+
+
+client = MongoClient('mongodb://localhost:27017/')  
+db = client['crop_Recommendation_analysis'] 
+collection = db['crop_predictions']  
 
 @app.route('/')
 def home():
@@ -25,7 +34,6 @@ def predict():
     try:
         data = request.get_json()
 
-      
         N = float(data['N'])
         P = float(data['P'])
         K = float(data['K'])
@@ -34,13 +42,27 @@ def predict():
         ph = float(data['ph'])
         rainfall = float(data['rainfall'])
 
-
         input_features = [[N, P, K, temperature, humidity, ph, rainfall]]
         prediction = model.predict(input_features)[0]
 
-        
         crop_name = label_to_crop.get(int(prediction), "Unknown Crop")
 
+        
+        data_to_store = {
+            'N': N,
+            'P': P,
+            'K': K,
+            'temperature': temperature,
+            'humidity': humidity,
+            'ph': ph,
+            'rainfall': rainfall,
+            'recommended_crop': crop_name
+        }
+
+    
+        collection.insert_one(data_to_store)
+
+     
         return jsonify({'crop': crop_name})
 
     except Exception as e:
